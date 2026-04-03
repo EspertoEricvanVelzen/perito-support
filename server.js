a74fc02e46427aed13bee4f9c13d0fb0fcabee08
Eric van Velzen <eric.vanvelzen@esperto.one>
2026-04-03 22:35:58 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const { loadContent, getPage, getInstrumenten, searchContent } = require('./lib/markdown');
